291821f084fc9c2023d96ad6df9607bc3b90399e
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2019-05-07 15:55:45 +0200

    added file renamer

--- instaDownloader_console.py
+++ instaDownloader_console.py
@@ -1,20 +1,29 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import subprocess
+import os
 
 def downlaod(imgUrl):
     request = requests.get(imgUrl)
     resault = request.content
     soup = bs(resault, 'lxml')
-    for divdata in soup.findAll('meta', {"property": "og:image"}):
-        for img in divdata.find_all('a'):
-            print(img)
+    for divdata in soup.findAll("meta",  property="og:image"):
+        imgSrc = divdata['content']
+        print(imgSrc)
+        argument = 'wget --no-check-certificate ' + imgSrc
+        subprocess.call(argument, shell=False)
 
 
-downlaod('https://www.instagram.com/p/BvynZtUgJVJ/?hl=en')
+def renameFild(origName, newName):
+    newName = newName + ".png"
+    os.rename(origName, newName)
 
+renameFild('test.com', 'oh_boi')
 
 '''
+working:
+for divdata in soup.findAll('meta', {"property": "og:image"}):
+
 https://www.instagram.com/p/BvynZtUgJVJ/?hl=en
 
 for divdata in soup2.findAll('div', {"class": "preview"}):
