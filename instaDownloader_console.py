474625ea67c942ca0520cbf3b303a8830cda3b15
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2019-05-07 15:11:24 +0200

    actually wrote some working code

--- instaDownloader_console.py
+++ instaDownloader_console.py
@@ -1,5 +1,16 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import subprocess
+
+def downlaod(imgUrl):
+    request = requests.get(imgUrl)
+    resault = request.content
+    soup = bs(resault, 'lxml')
+    links = soup.find_all("a")
+
+    print(links)
 
 
-print('hello world!')
+url = input('URL to post >>> ')
+
+downlaod(url)
