4850e26b8d966615b5b9edbbbbe557426addc531
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2019-05-07 15:41:56 +0200

    HA, found the link to the post

--- instaDownloader_console.py
+++ instaDownloader_console.py
@@ -6,9 +6,8 @@ def downlaod(imgUrl):
     request = requests.get(imgUrl)
     resault = request.content
     soup = bs(resault, 'lxml')
-    for divdata in soup.findAll('span', {"id": "react-root"}):
-        for links in divdata.findAll('section', {"calss": "_9eogI E3X2T"}):
-            print(links)
+    for divdata in soup.findAll('meta', {"property": "og:image"}):
+        print(divdata)
 
 
 downlaod('https://www.instagram.com/p/BvynZtUgJVJ/?hl=en')
@@ -21,5 +20,7 @@ for divdata in soup2.findAll('div', {"class": "preview"}):
     for getatag in divdata.findAll('a', {'class': 'zoom'}):
         for getimgtag in getatag.findAll('img',src=True):
             print getimgtag['src']
+
+<meta property="og:image" content="https://scontent-waw1-1.cdninstagram.com/vp/c1407f2b38c9983f569881bf0fe483cb/5D55E275/t51.2885-15/e35/52626401_648321182275488_9133271219251416285_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com" />
 
 '''
