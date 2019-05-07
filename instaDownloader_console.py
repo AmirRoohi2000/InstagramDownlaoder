87e43047f27caf3a3258c79f92c63c2f325f6bd2
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2019-05-07 15:46:30 +0200

    more helpful comments

--- instaDownloader_console.py
+++ instaDownloader_console.py
@@ -7,7 +7,8 @@ def downlaod(imgUrl):
     resault = request.content
     soup = bs(resault, 'lxml')
     for divdata in soup.findAll('meta', {"property": "og:image"}):
-        print(divdata)
+        for img in divdata.find_all('a'):
+            print(img)
 
 
 downlaod('https://www.instagram.com/p/BvynZtUgJVJ/?hl=en')
@@ -22,5 +23,12 @@ for divdata in soup2.findAll('div', {"class": "preview"}):
             print getimgtag['src']
 
 <meta property="og:image" content="https://scontent-waw1-1.cdninstagram.com/vp/c1407f2b38c9983f569881bf0fe483cb/5D55E275/t51.2885-15/e35/52626401_648321182275488_9133271219251416285_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com" />
+
+
+title = soup.find("meta",  property="og:title")
+url = soup.find("meta",  property="og:url")
+
+print(title["content"] if title else "No meta title given")
+print(url["content"] if url else "No meta url given")
 
 '''
