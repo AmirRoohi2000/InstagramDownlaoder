082cbf738c1ecbbeb17d96716d9524126b7b107e
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2019-05-29 22:24:23 +0200

    optimised console version also added the latest code to it from GUI version

--- instaDownloader_console.py
+++ instaDownloader_console.py
@@ -3,24 +3,24 @@ from bs4 import BeautifulSoup as bs
 import os
 import urllib.request
 
-
 def download(imgUrl, name):
-    request = requests.get(imgUrl)
-    renault = request.content
-    soup = bs(renault, 'lxml')
-    for divData in soup.findAll("meta", property="og:image"):
-        imgSrc = divData['content']
-        fileName = 'Downloads/' + name + '.png'
-        urllib.request.urlretrieve(imgSrc, fileName)
+    try:
+        request = requests.get(imgUrl)
+        resault = request.content
+        soup = bs(resault, 'lxml')
+        if soup.findAll("meta", property="og:video"):
+            for divData in soup.findAll("meta", property="og:video"):
+                imgSrc = divData['content']
+                fileName = 'InstaDownloads/' + name + '.mp4'
+                urllib.request.urlretrieve(imgSrc, fileName)
+        elif soup.findAll("meta", property="og:image"):
+            for divData in soup.findAll("meta", property="og:image"):
+                imgSrc = divData['content']
+                fileName = 'InstaDownloads/' + name + '.png'
+                urllib.request.urlretrieve(imgSrc, fileName)
 
-
-''' obsolete code, but might be useful for later
-def renameFile(newName):
-    newName = newName + ".png"
-    for files in os.listdir(os.curdir):
-        if ".com" in files:
-            os.rename(files, newName)
-'''
+    except Exception as ex:
+        print(str(ex))
 
 
 def checkFolders():
@@ -31,33 +31,7 @@ def checkFolders():
 
 checkFolders()
 
-# url = input('URL to Post >>> ')
+url = input('URL to Post >>> ')
 name = input('Name >>> ')
 
-download('https://www.instagram.com/p/BwuV94-gd6E/', name)
-
-# renameFile('BOI')
-
-# 'https://www.instagram.com/p/Bwz5CF7AgJf/'
-
-'''
-working:
-for divdata in soup.findAll('meta', {"property": "og:image"}):
-
-https://www.instagram.com/p/BvynZtUgJVJ/?hl=en
-
-for divdata in soup2.findAll('div', {"class": "preview"}):
-    for getatag in divdata.findAll('a', {'class': 'zoom'}):
-        for getimgtag in getatag.findAll('img',src=True):
-            print getimgtag['src']
-
-<meta property="og:image" content="https://scontent-waw1-1.cdninstagram.com/vp/c1407f2b38c9983f569881bf0fe483cb/5D55E275/t51.2885-15/e35/52626401_648321182275488_9133271219251416285_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com" />
-
-
-title = soup.find("meta",  property="og:title")
-url = soup.find("meta",  property="og:url")
-
-print(title["content"] if title else "No meta title given")
-print(url["content"] if url else "No meta url given")
-
-'''
+download(url, name)
